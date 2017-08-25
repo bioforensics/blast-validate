3ea2e5322a86abed538b26a27e6b1dfc110efbb4
dsommer <ddsommer@gmail.com>
2017-08-25 14:56:58 -0400

    intial import of scripts directory

---
+++ scripts/blastValidateInMemory.pl
@@ -0,0 +1,570 @@
+#! /usr/bin/perl
+#
+# This script filters BLAST hits based on input parameters and then classifies the sequence
+# using the LCA algorithm, reporting the target taxon if the classification is a child of the
+# target taxon, or the exact classification otherwise.
+#
+# For context, see the following publication:
+# "BLAST-based validation of metagenomic sequence assignments"
+#
+# Run script without any arguments to see detailed usage.
+#
+
+use strict;
+
+use Getopt::Long;
+
+use lib (`ktGetLibPath`);
+use KronaTools;
+
+my $minCov;
+my $minId;
+my $maxEval;
+my $minLength = 0;
+my $taxId = 0;
+my $bitWin;
+my $exclude;
+my $organism;
+
+GetOptions
+(
+ "c=s" => \$minCov,
+ "l=s" => \$minLength,
+ "i=s" => \$minId,
+ "t=i" => \$taxId,
+ "b=s" => \$bitWin,
+ "x" => \$exclude,
+ "o=s" => \$organism,
+ "e=s" => \$maxEval
+);
+
+if ( @ARGV < 1 )
+{
+    print STDERR "blastValidate.pl [options] <fasta> [<blast>] [<output>]\n";
+    print STDERR "   -o str  organism (\"ba\", \"cb\", \"yp\")\n";
+    print STDERR "   -c %    min coverage [0] or <value1>,<value2>,...\n";
+    print STDERR "   -l int  min alignment length or <value1>,<value2>,...\n";
+    print STDERR "   -i %    min % ID [0]  or <value1>,<value2>,...\n";
+    print STDERR "   -e num  log10 of max e-value [inf] <value1>,<value2>,...\n";
+    print STDERR "   -t int  target taxon ID [0]\n";
+    print STDERR "   -b num  bit score window for ties [0]  or <value1>,<value2>,...\n";
+    print STDERR "   -x      exclude hits to the \"other sequences\" clade\n";
+    print STDERR "\n";
+    print STDERR "Filters BLAST hits based on input parameters and then classifies the sequence using\n";
+    print STDERR "the LCA algorithm, reporting the target taxon if the classification is a child of\n";
+    print STDERR "the target taxon, or the exact classification otherwise.\n";
+    print STDERR "\n";
+    print STDERR "Organism defaults (-c, -i, -b, and -e can be overridden):\n";
+    print STDERR "   ba (B. anthracis): -c 0 -i 0 -b 8 -e -64 -x -t 1392\n";
+    print STDERR "   cb (C. botulinum): -c 0 -i 0 -b 8 -e -64 -x -t 1491\n";
+    print STDERR "   yp (Y. pestis):    -c 0 -i 0 -b 8 -e -64 -x -t 632\n";
+    print STDERR "\n";
+    print STDERR "   Defaults were chosen to maximize sensitivity while retaining perfect\n";
+    print STDERR "   precision, using a ground truth from simulated reads.\n";
+    print STDERR "\n";
+    print STDERR "<blast>  defaults to <fasta>.blastn\n";
+    print STDERR "<output> defaults to <fasta>.<params>.lca, where <params> describes parameters.\n";
+    
+    exit;
+}
+
+if ( $organism eq 'ba')
+{
+    if ( ! defined $minCov )
+    {
+	$minCov = 0;
+    }
+    
+    if ( ! defined $minId )
+    {
+	$minId = 0;
+    }
+    
+    if ( ! defined $bitWin )
+    {
+	$bitWin = 8;
+    }
+
+    if ( ! defined $maxEval )
+    {
+	$maxEval = -64;
+    }
+    
+    $taxId = 1392;
+    $exclude = 1;
+}
+elsif ( $organism eq 'cb')
+{
+    if ( ! defined $minCov )
+    {
+	$minCov = 0;
+    }
+    
+    if ( ! defined $minId )
+    {
+	$minId = 0;
+    }
+    
+    if ( ! defined $bitWin )
+    {
+	$bitWin = 8;
+    }
+
+    if ( ! defined $maxEval )
+    {
+	$maxEval = -64;
+    }
+    
+    $taxId = 1491;
+    $exclude = 1;
+}
+elsif ( $organism eq 'yp')
+{
+    if ( ! defined $minCov )
+    {
+	$minCov = 0;
+    }
+    
+    if ( ! defined $minId )
+    {
+	$minId = 0;
+    }
+    
+    if ( ! defined $bitWin )
+    {
+	$bitWin = 8;
+    }
+
+    if ( ! defined $maxEval )
+    {
+	$maxEval = -64;
+    }
+    
+    $taxId = 632;
+    $exclude = 1;
+}
+elsif ( defined $organism )
+{
+    print STDERR "ERROR: unrecognized organism (\"$organism\") given to -o\n";
+    exit 1;
+}
+
+my ($fasta, $blast, $lca) = @ARGV;
+
+if ( ! defined $blast )
+{
+    $blast = "$fasta.blastn";
+}
+
+my $params = "$fasta.c$minCov\_i$minId\_l$minLength\_b$bitWin" . (defined $maxEval ? "_e$maxEval" : "") . ($exclude ? "_x" : "");
+
+my %lengths = getSequenceLengths($fasta);
+
+print STDERR "Loading taxonomy...\n";
+
+setOption('noRank', 1);
+
+loadTaxonomy();
+
+my %idsFiltered;
+my %noHits;
+my %minLen;
+
+my $filtered = "$params.filt";
+
+my $filter = ! -e "$filtered" || (stat($blast))[9] > (stat("$filtered"))[9];
+
+print STDERR "Read in blast output...\n";
+open BLAST, $blast or die $blast;
+my @lines = <BLAST>;
+close BLAST;
+
+print STDERR " start search with $bitWin(bitwin), $minLength(len), $minCov(cov), $minId(id), $maxEval(eval) \n";
+my @lengths = split /,/, $minLength;
+foreach my $l ( @lengths ) {
+    my @covs = split /,/, $minCov;
+    foreach my $c ( @covs ) {
+	my @ids = split /,/, $minId;
+	foreach my $i ( @ids ) {
+	    if (defined $maxEval && $maxEval ne '') { 
+		my @evs = split /,/, $maxEval;
+		foreach my $e ( @evs ) {
+		    print STDERR " run blast_validate with $bitWin(bitwin), $l(len), $c(cov), $i(id), $e(eval) \n";
+		    validate_blast(\@lines, $bitWin, $l, $c, $i, $e);
+		}
+	    } else {
+		print STDERR " run blast_validate with $bitWin(bitwin), $l(len), $c(cov), $i(id), $maxEval(eval) \n";
+		validate_blast(\@lines, $bitWin, $l, $c, $i, $maxEval);
+	    }
+	}
+    }
+}
+print STDERR "done.\n";
+exit;
+
+
+## BEGIN SUBROUTINES ##
+
+sub classifyBlastArr
+{
+    # taxonomically classifies BLAST results based on LCA (or random selection)
+    # of 'best' hits.
+    #
+    # Options used: bitScore, factor, include, percentIdentity, random, score
+    
+    my # parameters
+	(
+	 $blastArr, # array with tabular BLAST results
+	 
+	 # hash refs to be populated with results (keyed by query ID)
+	 #
+	 $taxIDs,
+	 $scores
+	) = @_;
+    
+    # open BLAST, "<$fileName" or ktDie("Could not open $fileName\n");
+    
+    my $lastQueryID;
+    my $topScore;
+    my $topEVal;
+    my $ties;
+    my $taxID;
+    my %lcaSet;
+    my $totalScore;
+    my $zeroEVal;
+    
+    for my $line ( @{$blastArr} )
+    {
+	
+	chomp $line;
+	# print STDERR $line . " threshold " . getOption('threshold') . " \n";
+	
+	if ( $line =~ /^#/ )
+	{
+	    if ( $line =~ /Query: ([\S]+)/ )
+	    {
+		# Initialize taxID and score in case this query has no hits
+		$taxIDs->{$1} = -1;
+		$scores->{$1} = 0;
+	    }
+	    next;
+	}
+	
+	my
+	    (
+	     $queryID,
+	     $hitID,
+	     $identity,
+	     $length,
+	     $mismatches,
+	     $gaps,
+	     $queryStart,
+	     $queryEnd,
+	     $subjectStart,
+	     $subjectEnd,
+	     $eVal,
+	     $bitScore
+	    ) = split /\t/, $line;
+
+	# print "query ID: $queryID  last query ID: $lastQueryID\n";
+	
+	if ( $queryID ne $lastQueryID )
+	{
+	    if
+		(
+		 ! defined $lastQueryID &&
+		 ! defined $taxIDs->{$queryID} &&
+		 getOption('include')
+		)
+	    {
+		ktWarn("-i specified but array does not contain comment lines. Queries with no hits will not be included for this file."); 
+	    }
+	    
+	    if (  $ties )
+	    {
+		# add the chosen hit from the last queryID
+		
+		if ( ! getOption('random') )
+		{
+		    $taxID = taxLowestCommonAncestor(keys %lcaSet)
+		}
+		
+		$taxIDs->{$lastQueryID} = $taxID;
+		$scores->{$lastQueryID} = $totalScore / $ties;
+	    }
+	    
+	    $ties = 0;
+	    $totalScore = 0;
+	    %lcaSet = ();
+	}
+	
+	if ( ! defined $hitID )
+	{
+	    last; # EOF
+	}
+	
+	my $acc = getAccFromSeqID($hitID);
+	
+	if ( ! defined $acc )
+	{
+	    $lastQueryID = $queryID;
+	    next;
+	}
+	
+	if # this is a 'best' hit if...
+	    (
+	     $queryID ne $lastQueryID || # new query ID (including null at EOF)
+	     $bitScore >= $topScore - getOption('threshold') || # within score threshold
+	     getOption('factor') && $eVal <= getOption('factor') * $topEVal # within e-val factor
+	    )
+	{
+	    # add score for average
+	    #
+	    if ( getOption('percentIdentity') )
+	    {
+		$totalScore += $identity;
+	    }
+	    elsif ( getOption('bitScore') )
+	    {
+		$totalScore += $bitScore;
+	    }
+	    else
+	    {
+		if ( $eVal > 0 )
+		{
+		    $totalScore += (log $eVal) / log 10;
+		}
+		else
+		{
+		    $totalScore += $KronaTools::minEVal;
+		    $zeroEVal = 1;
+		}
+	    }
+
+	    $ties++;
+	    
+	    if # use this hit if...
+		(
+		 ! getOption('random') || # using LCA
+		 $queryID ne $lastQueryID || # new query ID
+		 int(rand($ties)) == 0 # randomly chosen to replace other hit
+		)
+	    {
+		my $newTaxID = getTaxIDFromAcc($acc);
+		
+		if ( ! $newTaxID || ! taxIDExists($newTaxID) )
+		{
+		    $newTaxID = 1;
+		}
+		
+		if ( getOption('random') )
+		{
+		    $taxID = $newTaxID;
+		}
+		else
+		{
+		    $lcaSet{$newTaxID} = 1;
+		}
+	    }
+	}
+	
+	if ( $queryID ne $lastQueryID )
+	{
+	    $topScore = $bitScore;
+	    $topEVal = $eVal;
+	}
+	
+	$lastQueryID = $queryID;
+    }
+    
+    if ( $zeroEVal )
+    {
+	ktWarn("Array had e-values of 0. Approximated log[10] of 0 as $KronaTools::minEVal.");
+    }
+}	
+
+# takes parsed blast and bitWin string
+sub validate_blast {
+    my # parameters
+	(
+	 $blast_lines, # array with tabular BLAST results
+	 $bitWin,
+	 $minLength,
+	 $minCov,
+	 $minId,
+	 $maxEval,
+	) = @_;
+
+    my @filterArr;
+    my @idOrder;
+    print STDERR "filtering hits by minLength, minCov, minId...\n";
+    my $queryIdLast;
+
+    for my $line (@{$blast_lines}) {
+	chomp $line;
+
+	if ( $line =~ /^\#/ ) {
+	    push @filterArr, $line;
+	    
+	    if ( $line =~ /Query: ([\S]+)/ ) {
+		push @idOrder, $1;
+	    }
+	    elsif ( $line eq "# 0 hits found" ) {
+		$noHits{$idOrder[-1]} = 1;
+	    }
+	    
+	    next;
+	}
+
+	my
+	    (
+	     $queryId,
+	     $hitId,
+	     $identity,
+	     $length,
+	     $mismatches,
+	     $gaps,
+	     $queryStart,
+	     $queryEnd,
+	     $subjectStart,
+	     $subjectEnd,
+	     $eVal,
+	     $bitScore,
+	     $slen
+	    ) = split /\t/, $line;
+
+	if ( $idsFiltered{$queryId} )
+	{
+	    next;
+	}
+
+	if ( ! defined $minLen{$queryId} )
+	{
+	    $minLen{$queryId} = $slen;
+	}
+
+	if
+	    (
+	     $queryId eq $queryIdLast ||
+	     $filter &&
+	     $identity >= $minId &&
+	     ($queryEnd - $queryStart + 1) / $lengths{$queryId} * 100 >= $minCov &&
+	     (! defined $maxEval || log($eVal)/log(10) <= $maxEval) &&
+	     $length >= $minLength
+	    )
+	{
+	    my $taxon = getTaxIDFromAcc(getAccFromSeqID($hitId));
+	    if ( ! $exclude || ! taxContains(28384, $taxId) )
+	    {
+		my $taxName = getTaxName($taxon);
+		push @filterArr, "$line\t$taxName";
+		
+	    }
+	}
+	else
+	{
+	    $idsFiltered{$queryId} = 1;
+	}
+	
+	$queryIdLast = $queryId;
+    }
+
+    print STDERR "applying LCA algorithm to hits...";
+    my @bits = split /,/, $bitWin;
+    foreach my $bit ( @bits ) {
+	my %taxIds;
+	my %scores;
+
+	setOption('threshold', $bit);
+
+	# push a null string onto the end of filterArr so Krona code (which previously read from a file) works properly
+	push @filterArr, "";
+	classifyBlastArr(\@filterArr, \%taxIds, \%scores);
+	$lca = "$fasta.c$minCov\_i$minId\_l$minLength\_b$bit\_e" . (defined $maxEval ? $maxEval : "none") . ".lca";
+	# print STDERR "output $bit to $lca...\n";
+	
+	open LCA, ">$lca";
+	
+	print LCA "# Minimum query coverage:     $minCov\%\n";
+	print LCA "# Minimum alignment length:   $minLength\n";
+	print LCA "# Minimum identity:           $minId\%\n";
+	print LCA "# Maximum e-value (log10):    " . (defined $maxEval ? $maxEval : "none") . "\n";
+	print LCA "# Target taxon:               $taxId\n";
+	print LCA "# LCA bit score range:        $bit\n";
+	print LCA "# Exclude \"other sequences\":  " . ($exclude ? 'yes' : 'no') . "\n";
+	print LCA "#\n";
+	print LCA "# query	taxon	name	kingdom	best-hit-subj-len\n";
+	
+	foreach my $id ( @idOrder )
+	{
+	    if ( $noHits{$id} )
+	    {
+		print LCA "$id\t-\tno hits\n";
+	    }
+	    elsif ( $taxIds{$id} == -1 )
+	    {
+		print STDERR "$id\t-\tfiltered\n";
+		print LCA "$id\t-\tfiltered\n";
+	    }
+	    elsif ( $taxId && taxContains($taxId, $taxIds{$id}) )
+	    {
+		print LCA "$id\t$taxId\t" . getTaxName($taxId) . "\n";
+	    }
+	    else
+	    {
+		my $kingdom = $taxIds{$id};
+		
+		while ( $kingdom > 1 && getTaxRank($kingdom) !~ /kingdom/ )
+		{
+		    $kingdom = getTaxParent($kingdom);
+		}
+		
+		print LCA "$id\t$taxIds{$id}\t" . getTaxName($taxIds{$id}) . "\t(" . getTaxName($kingdom) . ")\t$minLen{$id}\n";
+	    }
+	}
+	
+	close LCA;
+    }
+}
+
+sub getSequenceLengths
+{
+    my ($fileName) = @_;
+    
+    open FILE1, "<$fileName" or die "Could not open $fileName";
+    
+    my $tag;
+    my $length;
+    my %lengths;
+    
+    while (my $line = <FILE1>)
+    {
+	if ( $line =~ /^#/ )
+	{
+	    next;
+	}
+	
+	if ( $line =~ /> *([^ ]*).*\n/)
+	{
+	    if ( defined $tag )
+	    {
+		$lengths{$tag} = $length;
+	    }
+	    
+	    $tag = $1;
+	    $length = 0;
+	}
+	elsif ( defined $tag )
+	{
+	    chomp $line;
+	    
+	    $length += length $line;
+	}
+    }
+    
+    $lengths{$tag} = $length; # final length
+    
+    close FILE1;
+    
+    return %lengths;
+}
---
+++ scripts/evaluate_parameter_sweep.pl
@@ -0,0 +1,338 @@
+#!/usr/bin/perl
+#
+# This script evaluates the results of a blastValidate parameter sweep using either the "false negatives" approach only, 
+# or additionally the "near neighbor" approach.
+#
+# As input, takes 1) the NCBI taxonomy ID of the "target taxon" (the taxon whose classification is being evaluated);
+#                 2) the absolute path to a directory of target taxon LCA files (produced by the blastValidate script); and
+#                 3) optionally, the path to a directory of near neighbor LCA files (also produced by the blastValidate script)
+#
+# Currently this script uses the following optimality criterion:
+# first maximize precision, then maximize sensitivity for parameter value combinations yielding sensitivity > 0.
+#
+# For context, see the following publication:
+# "BLAST-based validation of metagenomic sequence assignments"
+#
+# usage: perl evaluate_parameter_sweep.pl taxon-id absolute-path-to-taxon-lca-files [absolute-path-to-near-neighbor-lca-files]
+#
+
+use lib (`ktGetLibPath`);
+use KronaTools;
+
+if(@ARGV < 2 || @ARGV > 3) {
+    print "usage: perl evaluate_parameter_sweep.pl taxon-id absolute-path-to-taxon-lca-files [absolute-path-to-near-neighbor-lca-files]\n";
+    exit;
+}
+
+$taxonid = $ARGV[0];
+$taxon_lcafiles_path = $ARGV[1];
+$near_neighbor_lcafiles_path = "";
+if(@ARGV == 3) {
+    $near_neighbor_lcafiles_path = $ARGV[2];
+}
+
+%taxon_lcafiles = ();
+%near_neighbor_lcafiles = ();
+
+# for determining "optimal" results
+$optimalFNstring = "\n";
+$optimalNNstring = "\n";
+$FNprec = 0.0;
+$FNsens = 0.0;
+$FNeval = 100;
+$FNbitscore = 0;
+$NNprec = 0.0;
+$NNsens = 0.0;
+$NNeval = 100;
+$NNbitscore = 0;
+
+# store lca files for taxon
+foreach $lcafile (glob ($taxon_lcafiles_path . "/*.lca")) {
+    $taxon_lcafiles{$lcafile} = 1;
+}
+
+if($near_neighbor_lcafiles_path ne "") {
+    # store lca files for near neighbor
+    foreach $lcafile (glob ($near_neighbor_lcafiles_path . "/*.lca")) {
+	$near_neighbor_lcafiles{$lcafile} = 1;
+    }
+}
+
+# write results to file
+$outputfile = "param_sweep_eval";
+open OUTPUT, ">$outputfile";
+
+if($near_neighbor_lcafiles_path ne "") { # we will have an extra column for precision based on false positives
+    print OUTPUT "TP\tFN\tFP\tsens (TP/read count)\tprec (TP/(TP+FN))\tprec (TP/(TP+FP))\tminlen\tminid\tmaxeval\tminbitscorediff\n";
+} else {
+    print OUTPUT "TP\tFN\tsens (TP/read count)\tprec (TP/(TP+FN))\tminlen\tminid\tmaxeval\tminbitscorediff\n";
+}
+
+# loop through lca files produced by blastValidate script
+# example file name: ba_ART_sim.fasta.c_i_l0_b16_e-80_x.lca
+foreach $lcafile (keys %taxon_lcafiles) {
+    if($lcafile =~ /.*\/(.*)\.c([0-9]*?)_i([0-9]*?)_l([0-9]*?)_b([0-9]*)/) {
+	$prefix = $1;
+	$coverage = $2;
+	$identity = $3;
+	$length = $4;
+	$bitscorediff = $5;
+	$evalue = "";
+
+	if($lcafile =~ /.*b[0-9]*_e(.*?)_/ || $lcafile =~ /.*b[0-9]*_e(.*?)\./) {
+	    $evalue = $1;
+	}
+
+	# make sure corresponding near neighbor lca file exists, if it's supposed to
+	$nnlcafile = "";
+	if($near_neighbor_lcafiles_path ne "") {
+	    $nnprefix = "";
+	    foreach $nnlcafiletemp (keys %near_neighbor_lcafiles) {
+		if($nnlcafiletemp =~ /(.*\/.*)\.c.*/) {
+		    $nnprefix = $1;
+		} else {
+		    print "couldn't get near neighbor lca file prefix from file: $nnlcafiletemp  exiting...\n";
+		    exit;
+		}
+	    }
+	    $nnlcafile = $nnprefix . ".c" . $coverage . "_i" . $identity . "_l" . $length . "_b" . $bitscorediff;
+	    if($evalue ne "") {
+		$nnlcafile .= "_e" . $evalue;
+	    }
+
+	    $foundnnlcafile = 0;
+	    foreach $nnlcafiletemp (keys %near_neighbor_lcafiles) {
+		if($nnlcafiletemp =~ /$nnlcafile/) {
+		    $nnlcafile = $nnlcafiletemp;
+		    $foundnnlcafile = 1;
+		    last;
+		}
+	    }
+
+	    if($foundnnlcafile == 0) {
+		print "didn't find near neighbor lca file with prefix: $nnlcafile  skipping...\n";
+		next;
+	    }
+	}
+
+	# parse lca file(s)
+	# example: # query	taxon	name	kingdom	best-hit-subj-len
+	# NC_003997.3-348480/1	1	root	(root)
+
+	$read_count = 0;
+	$taxon_count = 0;
+	$taxon_lineage_count = 0;
+	$no_hits_count = 0;
+	$taxon_count_in_nn = 0;
+
+	open LCA, $lcafile;
+	print "lca file: $lcafile\n";
+	while(<LCA>) {
+	    chomp;
+	    $line = $_;
+	    if(($line =~ /(.*?)\t(.*?)\t(.*?)\t.*$/ || $line =~ /(.*?)\t(.*?)\t(.*)$/) && $line !~ /query\ttaxon\tname/) {
+		$query = $1;
+		$taxon = $2;
+		$taxname = $3;
+		chomp($query, $taxon, $taxname);
+
+		$read_count++;
+		if($taxon eq $taxonid || taxContains($taxonid, $taxon)) { # adding the second clause in case taxon was not provided to blastValidate script
+		    $taxon_count++;
+		} elsif(taxContains($taxon, $taxonid)) {
+		    $taxon_lineage_count++;
+		} elsif($taxon eq "-") {
+		    $no_hits_count++;
+		} else {
+		    #print "false negative: $line\n";
+		}
+	    }
+	}
+	close LCA;
+
+	if($nnlcafile ne "") {
+	    open LCA, $nnlcafile;
+	    while(<LCA>) {
+		chomp;
+		$line = $_;
+		if(($line =~ /(.*?)\t(.*?)\t(.*?)\t.*$/ || $line =~ /(.*?)\t(.*?)\t(.*)$/) && $line !~ /query\ttaxon\tname/) {
+		    $query = $1;
+		    $taxon = $2;
+		    $taxname = $3;
+		    chomp($query, $taxon, $taxname);
+
+		    if($taxon eq $taxonid || taxContains($taxonid, $taxon)) { # adding the second clause in case taxon was not provided to blastValidate script
+			$taxon_count_in_nn++;
+		    }
+		}
+	    }
+	    close LCA;
+	}
+
+	$tp = $taxon_count;
+	$fn = $read_count - $taxon_count - $taxon_lineage_count - $no_hits_count;
+	chomp($tp);
+	chomp($fn);
+
+	if($coverage eq "") {
+	    $coverage = 0;
+	}
+
+	if($identity eq "") {
+	    $identity = 0;
+	}
+
+	if($evalue eq "") {
+	    $evalue = "inf";
+	}
+
+	# defining sensitivity so that it's useful/not misleading
+	if($tp > 0) {
+	    $sensitivity = $tp / $read_count;
+	} else {
+	    $sensitivity = 0;
+	}
+	# (as opposed to defining sensitivity this more standard way)
+	#if($tp + $fn > 0) {
+	#$sensitivity = $tp / ($tp + $fn);
+	#} else {
+	#$sensitivity = 0;
+	#}
+
+	# precision based on false negatives can always be computed
+	if($tp + $fn > 0) {
+	    $precisionfn = $tp / ($tp + $fn);
+	} else {
+	    $precisionfn = 1;
+	}
+	
+	if($nnlcafile ne "") {
+	    $fp = $taxon_count_in_nn;
+	    chomp($fp);
+	    
+	    # compute precision based on false positives (the "near neighbor" approach)
+	    if($tp + $fp > 0) {
+		$precisionfp = $tp / ($tp + $fp);
+	    } else {
+		$precisionfp = 1;
+	    }
+
+	    $outputstring = "$tp\t$fn\t$fp\t$sensitivity\t$precisionfn\t$precisionfp\t$length\t$identity\t$evalue\t$bitscorediff\n";
+	    print OUTPUT $outputstring;
+
+	    # debugging
+	    #print "precisionfp: $precisionfp\n";
+	    #print "sens: $sensitivity\n";
+	    #print "NNsens: $NNsens\n";
+	    #print "bitscorediff: $bitscorediff\n";
+	    #print "NNbitscore: $NNbitscore\n";
+	    #print "evalue: $evalue\n";
+	    #print "NNeval: $NNeval\n";
+
+	    # possibly update optimal near neighbor results
+	    if($sensitivity > 0 && $precisionfp > $NNprec) {
+		# print "better precision: $outputstring\n";
+		$NNprec = $precisionfp;
+		$NNsens = $sensitivity;
+		$NNbitscore = $bitscorediff;
+		$NNeval = $evalue;
+		$optimalNNstring = $outputstring;
+	    } elsif($sensitivity > 0 && $precisionfp == $NNprec && $sensitivity > $NNsens) {
+		# print "better sensitivity: $outputstring\n";
+		$NNsens = $sensitivity;
+		$NNbitscore = $bitscorediff;
+		$NNeval = $evalue;
+		$optimalNNstring = $outputstring;
+	    } elsif($sensitivity > 0 && $precisionfp == $NNprec && $sensitivity == $NNsens) {
+		if($bitscorediff > $NNbitscore) {
+		    # print "better bit score: $outputstring\n";
+		    $NNbitscore = $bitscorediff;
+		    $optimalNNstring = $outputstring;
+		} elsif($bitscorediff == $NNbitscore && $evalue < $NNeval) {
+		    # print "better evalue: $outputstring\n";
+		    $NNeval = $evalue;
+		    $optimalNNstring = $outputstring;
+		}
+	    }
+	} else {
+	    $outputstring = "$tp\t$fn\t$sensitivity\t$precisionfn\t$length\t$identity\t$evalue\t$bitscorediff\n";
+	    print OUTPUT $outputstring;
+	}
+
+	# possibly update optimal false negatives results
+	if($sensitivity > 0 && $precisionfn > $FNprec) {
+	    $FNprec = $precisionfn;
+	    $FNsens = $sensitivity;
+	    $FNbitscore = $bitscorediff;
+	    $FNeval = $evalue;
+	    $optimalFNstring = $outputstring;
+	} elsif($sensitivity > 0 && $precisionfn == $FNprec && $sensitivity > $FNsens) {
+	    $FNsens = $sensitivity;
+	    $FNbitscore = $bitscorediff;
+	    $FNeval = $evalue;
+	    $optimalFNstring = $outputstring;
+	} elsif($sensitivity > 0 && $precisionfn == $FNprec && $sensitivity == $FNsens) {
+	    if($bitscorediff > $FNbitscore) {
+		$FNbitscore = $bitscorediff;
+		$optimalFNstring = $outputstring;
+	    } elsif($bitscorediff == $FNbitscore && $evalue < $FNeval) {
+		$FNeval = $evalue;
+		$optimalFNstring = $outputstring;
+	    }
+	}
+    } else {
+	print "could not parse lca file: $lcafile  exiting...\n";
+	exit;
+    }
+}
+
+close OUTPUT;
+
+# print results for optimal parameter value combinations
+# example:
+# TP	FN	sens (TP/read count)	prec (TP/(TP+FN))	minlen	minid	maxeval	minbitscorediff
+# 20272	0	0.104943831857949	1	0	0	-64	8
+
+# check to see if we're using the near neighbor approach
+if($near_neighbor_lcafiles_path ne "") {
+    # print "optimal results for near neighbor approach: $optimalNNstring\n";
+    @chunks = split("\t", $optimalNNstring);
+    print "\noptimal results for near neighbor approach:\n";
+    print "\ttrue positives: " . $chunks[0] . "\n";
+    print "\tfalse negatives: " . $chunks[1] . "\n";
+    print "\tfalse positives: " . $chunks[2] . "\n";
+    print "\tsensitivity (TP/read count): " . $chunks[3] . "\n";
+    print "\tprecision (TP/(TP+FN)): " . $chunks[4] . "\n";
+    print "\tprecision (TP/(TP+FP)): " . $chunks[5] . "\n";
+    print "\tminimum alignment length: " . $chunks[6] . "\n";
+    print "\tminimum alignment percent identity: " . $chunks[7] . "\n";
+    print "\tmaximum E-value: " . $chunks[8] . "\n";
+    print "\tminimum bit score difference: " . $chunks[9] . "\n";
+}
+
+# we always have the false negatives approach
+# print "optimal results for false negatives approach: $optimalFNstring\n";
+@chunks = split("\t", $optimalFNstring);
+if($near_neighbor_lcafiles_path ne "") {
+    print "\noptimal results for false negatives approach:\n";
+    print "\ttrue positives: " . $chunks[0] . "\n";
+    print "\tfalse negatives: " . $chunks[1] . "\n";
+    print "\tsensitivity (TP/read count): " . $chunks[3] . "\n";
+    print "\tprecision (TP/(TP+FN)): " . $chunks[4] . "\n";
+    print "\tminimum alignment length: " . $chunks[6] . "\n";
+    print "\tminimum alignment percent identity: " . $chunks[7] . "\n";
+    print "\tmaximum E-value: " . $chunks[8] . "\n";
+    print "\tminimum bit score difference: " . $chunks[9] . "\n";
+} else {
+    print "\noptimal results for false negatives approach:\n";
+    print "\ttrue positives: " . $chunks[0] . "\n";
+    print "\tfalse negatives: " . $chunks[1] . "\n";
+    print "\tsensitivity (TP/read count): " . $chunks[2] . "\n";
+    print "\tprecision (TP/(TP+FN)): " . $chunks[3] . "\n";
+    print "\tminimum alignment length: " . $chunks[4] . "\n";
+    print "\tminimum alignment percent identity: " . $chunks[5] . "\n";
+    print "\tmaximum E-value: " . $chunks[6] . "\n";
+    print "\tminimum bit score difference: " . $chunks[7] . "\n";
+}
+
+exit;
